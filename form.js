9716866a0986d066dd91bbf0615b5578be73917b
venturads <albert@venturads.com>
2020-02-13 11:02:00 -0800

    fix getage function

--- form.js
+++ form.js
@@ -2,11 +2,11 @@ let app = document.getElementById('app');
 
 function ageCheck() {
     let getage = document.getElementById('age').value;
-    console.log(getage);
-    alert(getage);
-    if(age >= 21) {
+    //console.log(getage);
+    //alert(getage);
+    if(getage >= 21) {
         app.innerHTML = "you can drink";
-    }else if(age < 21){
+    }else if(getage < 21){
         app.innerHTML = "can't drink";
     }
 }
