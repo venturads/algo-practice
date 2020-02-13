bcf3f542a6ff7e2f1091933b1a6f30fbe1542318
venturads <albert@venturads.com>
2020-02-13 10:49:23 -0800

    update form

--- form.js
+++ form.js
@@ -1,6 +1,9 @@
 let app = document.getElementById('app');
+let getage = document.getElementById('age').value;
 
-function ageCheck(age) {
+function ageCheck(getage) {
+    console.log(getage);
+    alert(getage);
     if(age >= 21) {
         app.innerHTML = "you can drink";
     }else if(age < 21){
