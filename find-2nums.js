71b5103ace69532339a8d029ac6fe68bca46dec5
venturads <albert@venturads.com>
2020-02-14 23:51:17 -0800

    updated find 2 js

--- find-2nums.js
+++ find-2nums.js
@@ -2,3 +2,12 @@
 
 const arr = [2,3,11,4,6,21,5,9,10];
 
+function find2nums(arr) {
+    for(let i=0; i<arr.length;i++) {
+        for(let j=i+1; i<arr.length;j++) {
+            if((20/arr[i]) = arr[j]) {
+                return arr[j];
+            }
+        }
+    }
+}
