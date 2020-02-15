b6a4c4e32b7837441e3c2062cdc26e270d740c8d
venturads <albert@venturads.com>
2020-02-15 00:01:53 -0800

    clean code

--- find-2nums.js
+++ find-2nums.js
@@ -1,6 +1,6 @@
 // find two numbers in a array that multiply to a given number 20
 
-const arr = [2,3,11,4,6,21,5,9,10];
+const arr = [2,3,11,4,6,21,5,9];
 
 function find2nums(arr) {
     for(let i=0; i<arr.length;i++) {
@@ -8,6 +8,8 @@ function find2nums(arr) {
             console.log("num: ", arr[j]);
             if((20/arr[i]) == arr[j]) {
                 return console.log("numbers: ", arr[j] + " x " + arr[i] + " = 20");
+            }else{
+                return console.log("can't find number");
             }
         }
     }
