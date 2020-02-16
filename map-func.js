2d56f69dda28750c80201f6333897f523da0bb00
venturads <albert@venturads.com>
2020-02-15 22:47:46 -0800

    added hash map js

--- map-func.js
+++ map-func.js
@@ -1,7 +1,9 @@
 let user = new Map();
-let name = "kobe";
+let users = ([24, "kobe"], [41, "fish"], [33, "shaq"]);
 let output = document.getElementById('app');
 
-user.set(name.charCodeAt(0));
+user.set(name.charCodeAt(0), name, 41);
 
 console.log(user.get(name.charCodeAt(0)));
+
+output.innerHTML = user.get(name.charCodeAt(0));
