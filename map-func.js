c89227427f4c440b454abb6c37c0e719ec68e393
venturads <albert@venturads.com>
2020-02-15 22:54:43 -0800

    updated map func js

--- map-func.js
+++ map-func.js
@@ -1,5 +1,5 @@
 let user = new Map();
-let users = ([24, "kobe"], [41, "fish"], [33, "shaq"]);
+let users = [[24, "kobe"], [41, "fish"], [33, "shaq"]];
 let output = document.getElementById('app');
 
 user.set(name.charCodeAt(0), name, 41);
