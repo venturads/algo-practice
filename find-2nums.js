546f9260ac05b4e5d06bf98af4d3bd4ff57c6cba
venturads <albert@venturads.com>
2020-02-15 22:29:04 -0800

    added hash map js

--- find-2nums.js
+++ find-2nums.js
@@ -1,13 +1,12 @@
 // find two numbers in a array that multiply to a given number 20
 
-const arr = [2,3,11,4,6,21,5,9];
+const arr = [2,22,10,11,4,6,21,5,9,10];
 
 function find2nums(arr) {
     for(let i=0; i<arr.length;i++) {
         for(let j=i+1; i<arr.length;j++) {
-            console.log("num: ", arr[j]);
             if((20/arr[i]) == arr[j]) {
-                return console.log("numbers: ", arr[j] + " x " + arr[i] + " = 20");
+                return console.log("found numbers: ", arr[j] + " x " + arr[i] + " = 20");
             }else{
                 return console.log("can't find the number " + (20/arr[i]));
             }
---
+++ map-func.js
@@ -0,0 +1,4 @@
+let user = new user();
+let name = "kobe";
+
+user.set(name.charCodeAt(0));
