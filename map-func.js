cc40697d9489676d1205fad1235f2b9c0b889531
venturads <albert@venturads.com>
2020-02-15 23:07:13 -0800

    add multiple key value pairs map

--- map-func.js
+++ map-func.js
@@ -1,5 +1,6 @@
-let user = new Map();
+
 let users = [[24, "kobe"], [41, "fish"], [33, "shaq"]];
+let user = new Map(users);
 let output = document.getElementById('app');
 
 user.set(name.charCodeAt(0), name, 41);
@@ -9,5 +10,8 @@ user.set(name.charCodeAt(0), name, 41);
 output.innerHTML = user.get(name.charCodeAt(0));
 
 users.forEach(function(value, key) {
+    user.set(key, value);
     console.log(key + ' = ' + value);
 })
+
+console.log(user.get(24));
