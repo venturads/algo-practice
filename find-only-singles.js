4b25395adda58a13ac721ba3d8204c8154666a11
venturads <albert@venturads.com>
2020-02-17 19:27:43 -0800

    update algo

--- find-only-singles.js
+++ find-only-singles.js
@@ -1,10 +1,15 @@
 let arr = [2,3,5,2,5,8];
 let len = arr.length;
-let newarr = new Map();
+let newarr = [];
+let output = document.getElementById('app');
 function findSinglesNumbers(nums) {
-    for(let i=0; i<len; i++){
-        newarr.set(nums[i], 1);
+    for(let i of nums) {
         console.log(newarr);
-    } 
+    }
 }
 findSinglesNumbers(arr);
+let checkAge = function(value) {
+           return value == 5;
+        }
+
+console.log(arr.filter(checkAge));
