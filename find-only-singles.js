81e122c66debfd8eea4821e9ce82314b74c1e68f
venturads <albert@venturads.com>
2020-02-18 12:06:28 -0800

    update

--- find-only-singles.js
+++ find-only-singles.js
@@ -2,14 +2,10 @@ let arr = [2,3,5,2,5,8];
 let len = arr.length;
 let newarr = [];
 let output = document.getElementById('app');
-function findSinglesNumbers(nums) {
-    for(let i of nums) {
-        console.log(newarr);
-    }
-}
-findSinglesNumbers(arr);
-let checkAge = function(value) {
-           return value == 5;
-        }
 
-console.log(arr.filter(checkAge));
+const findSinglesNumbers = arr.forEach((item, index) => {
+    item == newarr[index] ? newarr.splice(0, 1) : newarr.push(item)
+    //console.log(newarr[index]);
+} );
+
+console.log(newarr);
