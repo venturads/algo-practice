0ad749e7aed0f22a4cdd6c2644b4cf9d10d53c96
venturads <albert@venturads.com>
2020-02-19 09:50:55 -0800

    add merge binary trees

--- merge-binary-trees.js
+++ merge-binary-trees.js
@@ -1,8 +1,12 @@
 const leftNode = [1,3,2,5];
 const rightNOde = [2,1,3,null,4,null,7];
+let len = leftNode.length, n;
 
 function mergeTrees(leftNode, rightNOde) {
-    
+    // for(n of leftNode) {
+
+    // }
+    console.log(leftNode.concat(rightNOde));
 }
 
 mergeTrees(leftNode, rightNOde);
