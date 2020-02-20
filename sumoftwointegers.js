3d142b73e95ee7f2070701edee9508eb565d4898
venturads <albert@venturads.com>
2020-02-19 23:33:14 -0800

    added push and concat to two arrays

--- sumoftwointegers.js
+++ sumoftwointegers.js
@@ -1,7 +1,13 @@
+let arr = [];
+let arr2 = [];
+
 var getSum = function(a, b) {
-    let arr = [];
-    while(arr.length < a || arr.length < b) {
+    while(arr.length < a) {
         arr.push(1);
     }
-    return arr.length;
+    while(arr2.length < b) {
+        arr2.push(1);
+    }
+    return arr.concat(arr2).length;
 }
+console.log(getSum(8,16));
