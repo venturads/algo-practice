6953cf7c2391fec67d70ef6a9de49c255493c374
venturads <albert@venturads.com>
2020-02-21 11:51:47 -0800

    added shorthand.js

---
+++ shortHand.js
@@ -0,0 +1,7 @@
+const app = document.getElementById('app');
+let nums = [2,33,52,42];
+
+// ternary operator
+const getNums = nums ? true : false;
+
+app.innerHTML = getNums;
--- simplehash.js
+++ simplehash.js
@@ -2,9 +2,12 @@ let myHash = new Map();
 myHash.set("k", "kobe");
 myHash.set("s", "shaq");
 
-//console.log(myHash.entries());
+console.log(myHash.get("k"));
 
 for(const x of myHash.entries()) {
     console.log(x);
   }
 
+const getH = myHash.get("m") ? true : false;
+
+console.log(getH);
