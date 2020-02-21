f5d6e01671b55a9757c2f2a2589f68a02eadb2f1
venturads <albert@venturads.com>
2020-02-21 12:02:24 -0800

    added for loops if state

--- shortHand.js
+++ shortHand.js
@@ -4,4 +4,16 @@ let nums = [2,33,52,42];
 // ternary operator
 const getNums = nums ? true : false;
 
+// present
+if(getNums);
+
+// for loop
+for(let n of nums) {
+    console.log("value: " + n);
+}
+
+for(let m in nums) {
+    console.log("index: " + m);
+}
+
 app.innerHTML = getNums;
