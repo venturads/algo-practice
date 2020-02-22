5198ea1013fb50c02e5360d536a6beb0c1871ca9
venturads <albert@venturads.com>
2020-02-22 00:05:08 -0800

    add binary search algo

--- binary-search.js
+++ binary-search.js
@@ -1,4 +1,8 @@
 let arr = [2,4,5,12,16,22,33,40,55];
-let len = arr.length;
+let len = arr.length; s = 0; e = len - 1;
 
-console.log(len);
+// while() {
+
+// }
+console.log(Math.round(len/2));
+console.log(s, e)
