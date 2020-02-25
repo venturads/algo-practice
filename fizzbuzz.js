191fc7750fc97babd057dcb6a1762092017503d1
venturads <albert@venturads.com>
2020-02-25 08:36:49 -0800

    added fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -8,8 +8,15 @@ var fizzBuzz = function(n) {
             n[i] = "Fizz";
         }else if(n[i] == 5) {
             n[i] = "Buzz";
-        }else if((n[i]/5) = 3) {
+        }else if((n[i]/5) == 3) {
             n[i] = "FizzBuzz";
         }
     }
 };
+
+var fizzBuzz = function(n) {
+    return new Array(n).fill(0).map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i);
+};
+
+let arr = [1,2,3,4,5,6,7,8,9,15];
+console.log(fizzBuzz(arr));
