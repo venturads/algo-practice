edc6b82ffcbb995efb789e36a0bb4f692ef16625
venturads <albert@venturads.com>
2020-02-24 17:18:23 -0800

    updated binary search

--- binary-search.js
+++ binary-search.js
@@ -1,23 +1,21 @@
 let arr = [2,4,5,12,16,22,33,40,55];
 let len = arr.length; s = 0; e = len - 1; mid = Math.round(e/2);
 console.log(len, s, e, mid);
-// function bs(x, arr) {
-//     while(s <= e) {
-//         mid = Math.round(e/2);
-//         if(arr[mid] == x) {
-//             return mid;
-//         }else if(arr[mid] > x) {
-//             e = mid-1;
-//             mid = Math.round(s - e)/2;
-//         }else if(arr[mid] < x) {
-//             s = mid + 1;
-//             mid = Math.round(s - e)/2;
-//         }else { 
-//             return -1;
-//         }
-//     }
-// }
+function bs(x, arr) {
+    while(s <= e) {
+        mid = Math.round(e/2);
+        if(arr[mid] == x) {
+            return mid;
+        }else if(arr[mid] > x) {
+            e = mid-1;
+            //mid = Math.round(s - e)/2;
+        }else{
+            e = mid - 1;
+        }
+    }
+    return -1;
+}
 
-// const ans = bs(5, arr);
+const ans = bs(22, arr);
 //console.log(Math.round(len/2));
 console.log(ans);
