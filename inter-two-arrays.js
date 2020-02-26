378455e8be4c19603e2ae66917a0972bfb3849d2
venturads <albert@venturads.com>
2020-02-26 12:21:49 -0800

    added intersection of two arrays

---
+++ inter-two-arrays.js
@@ -0,0 +1,25 @@
+let nums1 = [1,2,2,1];
+let nums2 = [2,2];
+let temp = new Map;
+
+function inter(nums1, nums2) {
+    const map = new Map();
+    for (const n of nums1) {
+        if (map.has(n)) {
+            map.set(n, map.get(n) + 1);
+        } else {
+            map.set(n, 1);
+        }
+    }
+    const result = [];
+    for (const n of nums2) {
+        if (map.has(n) && map.get(n) > 0) {
+            result.push(n);
+            map.set(n, map.get(n) - 1);
+        }
+    }
+    return result;
+};
+
+
+console.log(inter(nums1, nums2));
