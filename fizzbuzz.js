242e2746e67ddd60dd264880a60b23512e2afa87
venturads <albert@venturads.com>
2020-02-26 11:12:09 -0800

    added intersection of two arrays

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,17 +2,17 @@
  * @param {number} n
  * @return {string[]}
  */
-var fizzBuzz = function(n) {
-    for(let i in n) {
-        if(n[i] == 3) {
-            n[i] = "Fizz";
-        }else if(n[i] == 5) {
-            n[i] = "Buzz";
-        }else if((n[i]/5) == 3) {
-            n[i] = "FizzBuzz";
-        }
-    }
-};
+// var fizzBuzz = function(n) {
+//     for(let i in n) {
+//         if(n[i] == 3) {
+//             n[i] = "Fizz";
+//         }else if(n[i] == 5) {
+//             n[i] = "Buzz";
+//         }else if((n[i]/5) == 3) {
+//             n[i] = "FizzBuzz";
+//         }
+//     }
+// };
 
 var fizzBuzz = function(n) {
     return new Array(n).fill(0).map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i);
