eb5007fa81b36b22fb6fc8c155fd00626eff417f
venturads <albert@venturads.com>
2020-02-27 21:06:17 -0800

    add find intersection 3 arrays

--- find-intersection-3arrays.js
+++ find-intersection-3arrays.js
@@ -1,3 +1,17 @@
 let arr1 = [2,5,7,8,33];
 let arr2 = [1,4,8,22,33];
 let arr3 = [3,4,9,12,33];
+
+function findinter(arr1, arr2, arr3) {
+    let result = []; x = 0; y = 0; z = 0;
+    while(!oob()) {
+        if(arr1[x] == arr2[y] && arr3[z]) {
+            result.push(arr1[x]);
+            x++; y++; z++;
+        }else if(arr1[x] < arr2[y]) {
+            x++;
+        }else{
+            z++;
+        }
+    }
+}
