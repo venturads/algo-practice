2af8af1c2a9617fb7e21262a69dbaa2bc470f3d2
venturads <albert@venturads.com>
2020-02-29 20:56:27 -0800

    created basic hash map for memoization

--- memoization-basic1.js
+++ memoization-basic1.js
@@ -1,6 +1,12 @@
 let runCounter = 0;
+let runHash = new Map();
 
 function getTime(num) {
+    if(runHash.has(num)) {
+        return runHash.get(num);
+    }else {
+        runHash.set(num, num*num);
+    }
     runCounter++;
     return num*num + ", run: " + runCounter;
 }
