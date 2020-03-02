855f8d7b6650d3359c7b01140e86be14831d5bfc
venturads <albert@venturads.com>
2020-03-01 22:41:58 -0800

    added fib

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,13 @@
+let fibMap = new Map();
+
 function fib(n) {
-    if(x == 0 || x == 1) {
+    if(n == 1) {
+        return 0;
+    }else if(n == 2) {
         return 1;
     }else {
-        return (fib(x-1) + fib(x-2));
+        return (fib(n-1) + fib(n-2));
     }
 }
 
-console.log(fib(2));
+console.log(fib(8));
