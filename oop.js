3982452a0c10337b94ef2189e330576d9cbfb3fe
venturads <albert@venturads.com>
2020-03-03 23:06:54 -0800

    add oop js

--- oop.js
+++ oop.js
@@ -1,1 +1,13 @@
 let app = document.getElementById('app');
+
+function Person(name, age, music) {
+    this.name = name;
+    this.age = age;
+    this.music = music;
+
+    this.greet = () => app.innerHTML= ("Welcome: " + this.name + ", age:" + this.age + ", music: " + this.music[0])
+}
+
+person1 = new Person('Kobe', 41, ['rap', 'hip hop', 'jazz']);
+
+person1.greet();
