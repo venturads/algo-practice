5ea2ca4ff12e4c22ea5e3eba9e2df380fba464ab
venturads <albert@venturads.com>
2020-03-03 23:25:18 -0800

    add palindrome js

--- palindrome.js
+++ palindrome.js
@@ -1,5 +1,9 @@
 function pali(temp) {
-    return temp;
+    let len = temp.length;
+    if(temp[0] == temp[len-1]) {
+        return true + temp[0] + temp[len-1];
+    }
+    return false, len;
 }
 
-console.log(pali(str));
+console.log(pali('sossaos'));
