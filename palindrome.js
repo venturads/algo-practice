32261c2a66bfdcaa510ed518598302cc91103109
venturads <albert@venturads.com>
2020-03-06 13:37:51 -0800

    update pali

--- palindrome.js
+++ palindrome.js
@@ -1,12 +1,14 @@
 function pali(p) {
     let len = p.length;
-    for(let n of p) {
+    for(let n in p) {
         if(p[n] == p[len-1]) {
-            console.log(n);
+            console.log(p[n]);
             p[0] + p[len-1];
-        }console.log(n);
+        }else{
+            return false + " " + n + p[len-1];
+        }
     } 
-    return false;
+    return true;
 }
 
 console.log(pali('abba'));
