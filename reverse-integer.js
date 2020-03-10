20227e7f628e3bfc2691f9ba8ec3c5ef0e55529b
venturads <albert@venturads.com>
2020-03-09 19:29:33 -0700

    reverse integer

--- reverse-integer.js
+++ reverse-integer.js
@@ -1,11 +1,13 @@
-function reverse(x) {
-    if(x < 0) {
-        x = Math.abs(x);
-        x = x.toString().split('').reverse().join('');
-        return x = -parseInt(x);
-    }else{
-        return x.toString().split('').reverse().join('');
+function reverse(n) {
+    var reverseN = 0;
+    var sign = n < 0;
+    n = Math.abs(n);
+    while (n) {
+        reverseN = reverseN*10 + (n % 10);
+        n = Math.floor(n/10);
     }
-};
+    return reverseN > 0x7FFFFFFF ? 0 : sign ? -reverseN : reverseN;
+}
 
-console.log(reverse(-213));
+console.log(reverse(-123));
+console.log(reverse(1563847412));
