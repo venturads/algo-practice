592934aaa912cc6a77415cddb77c2f0bd6c3671c
venturads <albert@venturads.com>
2020-03-11 15:59:10 -0700

    update find dup

--- find-duplicates.js
+++ find-duplicates.js
@@ -3,9 +3,12 @@ let arr = [1,2,3,1];
 
 function findDup(arr) {
     for(let n = 0; n<arr.length; n++) {
-        if()
-        return arr[n];
-    }
+        if(sgl.get(n) == arr[n]){
+            arr[n];
+        }else{
+            sgl.set(arr[n], n++);
+        }
+    } return sgl.keys();
 }
 
 console.log(findDup(arr));
