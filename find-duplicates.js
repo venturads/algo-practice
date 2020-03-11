86b0630f1cbc944a7fcb11879371369ce731c82f
venturads <albert@venturads.com>
2020-03-11 15:05:19 -0700

    added find duplicates

--- find-duplicates.js
+++ find-duplicates.js
@@ -1,2 +1,11 @@
 let sgl = new Map();
+let arr = [1,2,3,1];
 
+function findDup(arr) {
+    for(let n = 0; n<arr.length; n++) {
+        if()
+        return arr[n];
+    }
+}
+
+console.log(findDup(arr));
