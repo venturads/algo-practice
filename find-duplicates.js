bf0232775d21881035555715313917e0348a331c
venturads <albert@venturads.com>
2020-03-11 16:05:36 -0700

    update find dup

--- find-duplicates.js
+++ find-duplicates.js
@@ -1,14 +1,21 @@
-let sgl = new Map();
-let arr = [1,2,3,1];
+function find_duplicate_in_array(arra1) {
+    var object = {};
+    var result = [];
 
-function findDup(arr) {
-    for(let n = 0; n<arr.length; n++) {
-        if(sgl.get(n) == arr[n]){
-            arr[n];
-        }else{
-            sgl.set(arr[n], n++);
-        }
-    } return sgl.keys();
+    arra1.forEach(function (item) {
+      if(!object[item])
+          object[item] = 0;
+        object[item] += 1;
+    })
+
+    for (var prop in object) {
+       if(object[prop] >= 2) {
+           result.push(prop);
+       }
+    }
+
+    return result;
+
 }
 
-console.log(findDup(arr));
+console.log(find_duplicate_in_array([1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6]));
