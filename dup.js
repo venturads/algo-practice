c7a2feef242a46afdf7d369679162bb33af8a812
venturads <albert@venturads.com>
2020-03-16 22:45:52 -0700

    update js

--- dup.js
+++ dup.js
@@ -2,15 +2,19 @@ let nums = [1,2,3,4,1,2,4,3];
 let newArr = new Map();
 
 function getDup(arr) {
+    let m = 1;
     for(let n=0; n< arr.length; n++) {
         //console.log(arr[n]);
         if(arr[n] != newArr.get(n)) {
-            newArr.set(arr[n], 1);
+            newArr.set(arr[n], m);
+        }else {
+            m++;
         }
     }
-     for(let i of newArr.entries()) {
-         if() {
-            console.log(i);
+     for(let i of newArr.keys()) {
+        console.log(i);
+         if(i >= 2) {
+            console.log(newArr.get(i));
          }
         
      }
